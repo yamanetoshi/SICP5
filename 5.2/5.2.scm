5d73f5c8ca67d416423218e463700bdeb340b5bb
YAMANE Toshiaki <yamanetoshi@gmail.com>
2014-11-05 22:14:27 +0900

    initial commit

---
+++ 5.2/5.2.scm
@@ -0,0 +1,11 @@
+(define (make-machine register-name ops controller-text)
+  (let ((machine (make-new-machine)))
+    (for-each (lambda (register-name)
+		((machine 'allocate-register) register-name))
+	      register-names)
+    ((machine 'install-operations) ops)
+    ((machine 'install-instruction-sequence)
+     (assenble controller-text machine))
+    machine))
+
+	      
